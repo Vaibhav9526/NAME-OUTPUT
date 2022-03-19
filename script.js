e86292c51b68066e1dcc7e5089d7d439d7864b1a
Vaibhav9526 <81508584+Vaibhav9526@users.noreply.github.com>
2022-03-19 13:24:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function Name(){
+    const OUTPUT = document.getElementById("Input1").value;
+    document.getElementById("Demo").innerHTML = " YOUR OUTPUT IS :- <BR> <BR> <BR>" + OUTPUT;
+    console.log("Kaisa laga Bhai :)")
+}
